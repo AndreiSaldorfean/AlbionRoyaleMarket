17e3931991ee50cbe8f412d4a0f60af8e5d8027d
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-04-06 22:12:54 +0300

    Fetched data from API and parsed it;need further work on multithreading

--- InputFiles/python/scripts/fetch_prices/fetch_prices.py
+++ InputFiles/python/scripts/fetch_prices/fetch_prices.py
@@ -1,14 +1,20 @@
 import json
 import requests
+import os
 import time#OBS:perf_counter() is prefered over time()
 QUALITY=1
+FILE_PATH="../..//output//json//"
+FILE_NAME=FILE_PATH+"fetched_prices.json"
 URL_LIMIT=4096#based on indication found at https://www.albion-online-data.com/
 URL_BEGIN="https://west.albion-online-data.com/api/v2/stats/Prices/"
-URL_END=".json?locations=Caerleon,Martlock,Bridgewatch,Lymhurst,Fort Sterling,Thetford&qualities=QUALITY"
+URL_END=".json?locations=Caerleon,Martlock,Bridgewatch,Lymhurst,Fort%20Sterling,Thetford&qualities=1"
 items_list=[]#used for storing information about item id,name,city
-final_items_list=[]#the file which will be transmited to the user
+with open(FILE_NAME,"w") as fout:#formating the .json file
+    fout.write("[")
+    fout.close()
+#============================================================================================    
 def generate_items_list():#used for gathering items ID
-    fin=open("items_info_list.json")
+    fin=open("../..//output//json//items_info_list.json")
     buffer=json.load(fin)
     buffer_length=len(buffer)
     for i in range(buffer_length):
@@ -16,18 +22,16 @@ def generate_items_list():#used for gathering items ID
     fin.close()
 def append_file(data_request):#used for appending data to the prices.json file
     buffer=data_request.json()
-    buffer=json.dumps(buffer,indent=1)
+    buffer=json.dumps(buffer,indent=4)
+    #json.dumps({ID:"VAL"},indent=4)
     buffer=buffer.replace("[","")
     buffer=buffer.replace("]",",")
-    with open("temp.json","a") as fout:
+    with open(FILE_NAME,"a") as fout:
         fout.write(buffer)
     fout.close()
 
 def data_fetching():#used for fetching data via API
     #without any threads or multiple processes,there are 40 requests
-    with open("temp.json","a") as fout:
-        fout.write("[")
-    fout.close()
     url_size=URL_LIMIT-len(URL_END)
     max_index=len(items_list)
     url=URL_BEGIN
@@ -38,13 +42,18 @@ def data_fetching():#used for fetching data via API
             index=index+1
         else:
             url=url[:-1]#removing "," for the last item
+            url=url+URL_END
             data_request=requests.get(url)
+            print(data_request.status_code)
             append_file(data_request)
             url=URL_BEGIN
-    with open("temp.json","a") as fout:
-        fout.write("]")
-    fout.close()
 generate_items_list()
 data_fetching()
-#final_items_list=json.load(open("temp.json"))
-#print(len(final_items_list))
+#============================================================================================
+with open(FILE_NAME, 'rb+') as file:#formating the .json file
+    file.seek(-1, os.SEEK_END)
+    file.truncate()
+    file.close()
+with open(FILE_NAME,"a") as fout:
+    fout.write("]")
+    fout.close()
---
+++ InputFiles/python/scripts/fetch_prices/price_parsing.py
@@ -0,0 +1,65 @@
+import json
+import os
+from datetime import datetime
+PRICES_FILE="../../output//json//fetched_prices.json"
+PARSED_PRICES_FILE="../../output//json//parsed_prices.json"
+NUM_CITIES=6#number of cities=number of instances when an item appears on the prices file
+fin=open(PRICES_FILE,"r")
+prices_list=json.load(fin)
+fin.close()
+#NOTE:Black Market and Merlyn's Rest are not taken into account
+#the time specified in "fetched_prices.json" is in ISO 8601 format
+def search_min_sell(index):
+    min_sell_price=9999999999
+    sell_date="1989"
+    city_buy="Aiud"
+    for i in range(NUM_CITIES):
+        if min_sell_price>prices_list[index+i]["sell_price_min"]:
+            min_sell_price=prices_list[index+i]["sell_price_min"]
+            sell_date=prices_list[index+i]["sell_price_min_date"]
+            sell_date=datetime.fromisoformat(sell_date)#converting string to ISO 8601 date
+            city_buy=prices_list[index+i]["city"]
+    return (min_sell_price,sell_date,city_buy)
+def search_max_buy(index):
+    max_sell_price=-9999999999
+    buy_date="1989"
+    city_sell="Aiud"
+    for i in range(NUM_CITIES):
+        if max_sell_price<prices_list[index+i]["buy_price_max"]:
+            max_sell_price=prices_list[index+i]["buy_price_max"]
+            buy_date=prices_list[index+i]["sell_price_min_date"]
+            buy_date=datetime.fromisoformat(buy_date)#converting string to ISO 8601 date
+            city_sell=prices_list[index+i]["city"]
+    return(max_sell_price,buy_date,city_sell)
+#================================================================================================
+'''
+    Due to the fact that the "fetched_prices.json" has the items placed in order
+    (eg:6 instances "T2_WOOD" in series and after that other 6 instances of other items),
+    parsing the file can be done by incrementing with six(or how many cities/places are to be considered)
+    and then to acces the id
+'''
+index=0
+fout=open(PARSED_PRICES_FILE,"w")
+fout.write("{\n")
+indices=len(prices_list)
+while index<indices:
+    buy_data=search_min_sell(index)
+    sell_data=search_max_buy(index)
+    temp={prices_list[index][ "item_id"]:{
+        "price_buy":buy_data[0],
+        "price_sell":sell_data[0],
+        "revision_buy":str((datetime.now()-buy_data[1]).seconds/60),
+        "revision_sell":str((datetime.now()-sell_data[1]).seconds/60),
+                        #the time difference is converted into minutes,then into a string
+        "quality":1,
+        "city_buy":buy_data[2],
+        "city_sell":sell_data[2]
+    }}
+    temp=json.dumps(temp,indent=4)
+    if index<indices-6:
+        temp=temp[2:-2]+",\n" # the opening and closing "{}" are removed from the list in order to format it properly
+    else:
+        temp=temp[2:-2]+"\n}\n"
+    fout.write(temp)
+    index=index+NUM_CITIES
+fout.close()
