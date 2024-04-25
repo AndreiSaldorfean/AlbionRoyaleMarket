542ff1d8ae67c45aefdc8350bfb9e434f51f7bd5
Andrei <andreisaldorfean@gmail.com>
2024-04-25 18:10:32 +0300

    Test1

--- InputFiles/python/scripts/items_info/get_item_tier.py
+++
@@ -1,8 +0,0 @@
-file = open("output/text/items_id.txt","r")
-out = open("output/text/items_tier.txt","w")
-
-for line in file:
-    out.write(line[1]+"\n")
-
-file.close()
-out.close()
--- albion_royale_market_website/src/App.js
+++ albion_royale_market_website/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
+import ItemsTable from './components/ItemsTable.js';
+import Menu from './components/Menu.js';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
+        <Menu></Menu>
+        <ItemsTable></ItemsTable>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,10 +21,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn eaf
         </a>
       </header>
+
     </div>
+    </>
   );
 }
 
